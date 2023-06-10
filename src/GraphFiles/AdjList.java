96582c43524b18c7167a2d78e7997f0209312275
JG <ladrenrights@gmail.com>
2023-06-09 20:50:26 -0700

    Implemented add edge

--- src/GraphFiles/AdjList.java
+++ src/GraphFiles/AdjList.java
@@ -25,6 +25,34 @@ public class AdjList
 	 */
 	public void addEdge(int u, int v, int weight) 
 	{
-		
+		if(invalidVertex(u) || invalidVertex(v)) 
+		{
+			throw new IllegalArgumentException("Vertex passed into function is out of valid range");
+		}
+		Edge currEdge = graph[u];
+		while(currEdge.next != null && currEdge.next.vertex < v) 
+		{
+			currEdge = currEdge.next;
+		}
+		if(currEdge.next != null && currEdge.next.vertex == v) 
+		{
+			currEdge.next.weight = weight;
+		}
+		else 
+		{
+			currEdge.next = new Edge(v,weight,currEdge.next);
+		}
+	}
+	
+	/*
+	 * Vertexes are labeled from 0 to (n - 1) where n is the number of nodes in the graph
+	 */
+	private boolean invalidVertex(int vertex) 
+	{
+		if(vertex < 0 || vertex >= graph.length) 
+		{
+			return true;
+		}
+		return false;
 	}
 }
