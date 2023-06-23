19d4fe1e0a0d89a3d8a5127b3d055c93e1bca48d
JG <ladrenrights@gmail.com>
2023-06-22 17:26:27 -0700

    forgot main file was in git ignore and started implementing driver code for main function

--- src/Application/Action.java
+++ src/Application/Action.java
@@ -1,5 +1,16 @@
 package Application;
 
+/*
+ * The purpose of the action class is to add the actions that users can do in the main function.
+ * How this will work is there will be an ArrayList of anonymous Action classes. In the main class I'll
+ * implement them with a string that describes the action and one function that will actually do that specific action.
+ * The class fields are all static so they will be visible to the anonymous action class. In the main application
+ * loop a for loop will go through each individual action and print the description and then do the action that 
+ * the user wants. 
+ * 
+ * I chose to do it this way because any other way would involve using switch statements or a bunch of if else statements
+ * and if I chose to change the order of actions later on I would have to alter the structure of the switch cases.
+ */
 public abstract class Action
 {
 	String actionDescription;
---
+++ src/Application/Main.java
@@ -0,0 +1,113 @@
+package Application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main
+{
+	static Scanner kb = new Scanner(System.in);
+	
+	static Scheduler scheduler;
+	
+	static Parser parsedFile;
+	
+	static Scanner inputFile;
+	
+	static ArrayList<Action> userActions = new ArrayList<>();
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		addUserActions();
+		getInputFile();
+		processFile();
+		while(scheduler.donePlanning() == false) 
+		{
+			promptUserForAction();
+		}
+		
+	}
+	
+
+	public static void getInputFile() 
+	{
+		String fileName = "";
+		System.out.print("Enter the the input file: ");
+		do 
+		{
+			fileName = kb.nextLine();
+			try
+			{
+				inputFile = new Scanner(new File(fileName));
+			} catch (FileNotFoundException e)
+			{
+				System.out.print("The entered file cannot be found. Please enter again:");
+			}
+		}while(inputFile == null);
+	}
+	
+	private static void processFile()
+	{
+		try 
+		{
+			parsedFile = new Parser(inputFile);
+			scheduler = new Scheduler(parsedFile);
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println("Please fix the error and reinput the file.");
+			System.exit(0);
+		}
+	}
+	
+	public static void promptUserForAction() 
+	{
+		for(int i = 0; i < userActions.size(); i++) 
+		{
+			System.out.println(i + ") " + userActions.get(i).actionDescription);
+		}
+		System.out.println("Enter the number corresponding to the action you want to take:");
+		int actionIndex = -1;
+		do 
+		{
+			actionIndex = getNumber();
+		}while(actionIndex < 1 || actionIndex >= userActions.size());
+		userActions.get(actionIndex - 1).doAction();
+	}
+	
+	public static void addUserActions() 
+	{
+		userActions.add(new Action()
+		{
+			
+			String actionDescription  = "Show schedule that has been planned so far";
+			public void doAction()
+			{
+				System.out.println(scheduler.getPlannedSchedule());
+			}
+		});
+	}
+	
+	public static int getNumber() 
+	{
+		int number = 0;
+		boolean numberPicked = false;
+		while(numberPicked == false) 
+		{
+			try
+			{
+				number = kb.nextInt();
+				numberPicked = true;
+			} 
+			catch (InputMismatchException e)
+			{
+				System.out.print("The string entered was not a number. Please enter again: ");
+			}
+
+		}
+		return number;
+	}
+}
