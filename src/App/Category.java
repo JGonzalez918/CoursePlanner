b2dc34089f70fc05ea68e586b969fcacf08cfeae
JG <ladrenrights@gmail.com>
2023-08-05 17:06:17 -0700

    Created category class

---
+++ src/App/Category.java
@@ -0,0 +1,16 @@
+package App;
+
+public class Category
+{
+	String categoryID; 
+	String categoryName;
+	int unitRequirement;
+	public Category(String categoryID, String categoryName, int unitRequirement)
+	{
+		this.categoryID = categoryID;
+		this.categoryName = categoryName;
+		this.unitRequirement = unitRequirement;
+	}
+	
+	
+}
