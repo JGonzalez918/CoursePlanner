ca2efd099995dd801cc736880f9920387df575ec
JG <ladrenrights@gmail.com>
2023-08-07 16:39:41 -0700

    Added new variables and methods that add functionalliy to nodes

--- src/App/InputScene.java
+++ src/App/InputScene.java
@@ -1,5 +1,11 @@
 package App;
 
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import DataStructures.Trie;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -53,6 +59,8 @@ public class InputScene
 	
 	private Button addCourse;
 	
+	private Button submitButton;
+	
 	private final static int COURSE_LABEL_ROW = 3;
 	
 	private final static int COURSE_INPUT_ROW = 4;
@@ -64,12 +72,24 @@ public class InputScene
 	private final static double PADDING = 1;
 
 	private static final double SPACING = 25;
+
+	private static final double SUBMITBUTTON_VERTICAL_PADDING = 5;
+
+	private static final double SUBMITBUTTON_HORIZANTAL_PADDING = 75;
+
+	private static final int SUBMITBUTTON_ROW = 6;
+
+	private static final int SUBMITBUTTON_COL = 5;
 	
+	private ArrayList<Category> categoryList;
+	
+	private Trie categoryIdTree;
 	
 	public InputScene(Stage primaryStage) 
 	{
 		this.primaryStage = primaryStage;
 		primaryStage.setResizable(false);
+		
 		initialize();
 		buildScene();
 		primaryStage.setScene(inputScene);
@@ -83,20 +103,56 @@ public class InputScene
 		buildCourseInput();
 		categoryButtonFunctionality();
 		addCourseButtonFunctionality();
+		addSubmitButton();
+	}
+
+
+	private void addSubmitButton()
+	{
+		submitButton = new Button("Submit");
+		submitButton.setPadding(new Insets(SUBMITBUTTON_VERTICAL_PADDING,SUBMITBUTTON_HORIZANTAL_PADDING,SUBMITBUTTON_VERTICAL_PADDING,SUBMITBUTTON_HORIZANTAL_PADDING));
+		
+		layout.add(submitButton, SUBMITBUTTON_COL, SUBMITBUTTON_ROW);
 	}
 
 
 	private void addCourseButtonFunctionality()
 	{
-		addCourse.setOnAction((notUsed) -> {
+		addCourse.setOnAction((click) -> {
+
+		});
+	}
+	
+	private void courseCategoryBox() 
+	{
+		courseCategoryBox.getEditor().setOnMouseClicked(event -> {
+			courseCategoryBox.getItems().clear();
+			ArrayList<Integer> matches = categoryIdTree.getNMatches("");
+			for(int i = 0; i < matches.size(); i++) 
+			{
+				courseCategoryBox.getItems().add(categoryList.get(matches.get(i)).toString());
+			}
+			courseCategoryBox.show();
 			
+		});
+		
+		courseCategoryBox.getEditor().setOnKeyTyped(event -> {
+			courseCategoryBox.getItems().clear();
+			String enteredString = courseCategoryBox.getEditor().getText();
+			
+			ArrayList<Integer> matches = categoryIdTree.getNMatches(enteredString);
+			for(int i = 0; i < matches.size(); i++) 
+			{
+				courseCategoryBox.getItems().add(categoryList.get(matches.get(i)).toString());
+			}
+			courseCategoryBox.show();
 		});
 	}
 
 	private void categoryButtonFunctionality()
 	{
 		addCategoryButton.setOnAction((notUsed) -> {	
-			String categoryAbbr = categoryAbbreviation.getText().strip();
+			String categoryAbbr = categoryAbbreviation.getText();//all caps no space function
 			String categoryName = categoryNameField.getText().strip();
 			String categoryUnitRequirement = categoryUnitField.getText().strip();
 
@@ -105,7 +161,9 @@ public class InputScene
 				return;
 			}
 			HBox categoryBox = makeHBox(categoryAbbr,categoryName, categoryUnitRequirement);
-			//TODO: add category name to category choice box
+			categoryList.add(new Category(categoryAbbr, categoryName, Integer.parseInt(categoryUnitRequirement)));
+			categoryIdTree.addString(categoryAbbr, categoryList.size() - 1);
+			
 			enteredCategories.getItems().add(categoryBox);
 			categoryAbbreviation.clear();
 			categoryNameField.clear();
@@ -194,6 +252,7 @@ public class InputScene
 		layout.add(addCourse, 10, COURSE_INPUT_ROW);
 		layout.add(enteredCourses,0,COURSE_LIST_ROW,MAX_COLUMS,1);
 		setCourseInputProperties();
+		courseCategoryBox();
 	}
 
 
@@ -249,5 +308,8 @@ public class InputScene
 		coursePrereqBox = new ComboBox<>();
 		enteredCourses = new ListView<>();
 		addCourse = new Button("Add Course");
+		categoryIdTree = new Trie();
+		categoryList = new ArrayList<>();
+		layout.setGridLinesVisible(true);
 	}
 }
